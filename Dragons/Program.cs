86ef73cf25bb47bfbe35dfe29326e9f8c78ae012
zhukotanskiyphilip <zhukotanskiyphilip@gmail.com>
2022-01-17 12:37:17 +0200

    Player created

--- Dragons/Program.cs
+++ Dragons/Program.cs
@@ -10,6 +10,16 @@ namespace Dragons
     {
         static void Main(string[] args)
         {
+            Start();
+        }
+
+        static void Start()
+        {
+            Console.Write("Dragons\n" +
+                "Once upon a time, in the depths of the mountains of a distant country, the first dragon was born.\n"
+                + "His name was...\n" +
+                "Enter your dragon's name: ");
+            Console.ReadLine();
         }
     }
 }
